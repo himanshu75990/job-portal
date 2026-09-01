fd7c1ee6326149fd75fb82e41fee81a73224f950
himanshu75990 <himanshu75990@gmail.com>
2026-09-01 23:48:28 +0530

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Job Portal</h1>
+      <p>subscribe to me</p>
+    </div>
+  )
+}
+
+export default App
+
